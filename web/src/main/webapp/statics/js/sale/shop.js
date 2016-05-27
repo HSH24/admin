2fe83ab4f953a39d5f09f50fb819b2de7d85a7b5
Jiakun Xu <jiakunxu@gmail.com>
2016-05-27 19:33:42 +0800

    <>?

--- web/src/main/webapp/statics/js/sale/shop.js
+++ web/src/main/webapp/statics/js/sale/shop.js
@@ -1,4 +1,25 @@
 myApp.onPageInit('sale.shop', function(page) {
+
+			myPicker = myApp.picker({
+						input : '#month_select',
+						rotateEffect : true,
+						cols : [{
+									textAlign : 'left',
+									values : ['2015年', '2016年']
+								}, {
+									values : ['1月', '2月', '3月', '4月', '5月',
+											'6月', '7月', '8月', '9月', '10月',
+											'11月', '12月']
+								}],
+						onChange : function(picker, values, displayValues) {
+							var a = 1;
+						}
+					});
+
+			$$('#aaabbbccc').on('click', function() {
+						myPicker.open();
+					});
+
 			sale_shop_stats();
 		});
 
