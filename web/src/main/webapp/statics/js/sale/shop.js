a40c4fa29f36e1c6f59b6378cb7adf475e7afbf1
Jiakun Xu <jiakunxu@gmail.com>
2016-05-30 14:23:55 +0800

    year month

--- web/src/main/webapp/statics/js/sale/shop.js
+++ web/src/main/webapp/statics/js/sale/shop.js
@@ -2,20 +2,22 @@ myApp.onPageInit('sale.shop', function(page) {
 			myApp.picker({
 						input : '#sale/shop/date',
 						rotateEffect : true,
-						cols : [{
-									textAlign : 'left',
-									values : ['2015年', '2016年']
-								}, {
-									values : ['1月', '2月', '3月', '4月', '5月',
-											'6月', '7月', '8月', '9月', '10月',
-											'11月', '12月']
-								}],
-						onChange : function(p, values, displayValues) {
-							// $$('#sale/shop/month').html(values[1] + '月');
-							// $$('#sale/shop/year').html(values[0] + '年');
+						formatValue : function(p, values, displayValues) {
+							return values[0] + '年' + values[1] + '月';
 						},
+						cols : [{
+									values : ['2015', '2016', '2017']
+								}, {
+									values : ['1', '2', '3', '4', '5', '6',
+											'7', '8', '9', '10', '11', '12']
+								}],
 						onOpen : function(p) {
-							p.setValue(['2015年', '5月']);
+							p.setValue([$$('#sale/shop/year').val(),
+									$$('#sale/shop/month').val()]);
+						},
+						onClose : function(p) {
+							$$('#sale/shop/year').val(p.value[0]);
+							$$('#sale/shop/month').val(p.value[1]);
 						}
 					});
 
